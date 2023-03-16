88c018e38e37623ab9753db7b4431116e63c9e9e
Dimitrios Politis <dimi.politis@yahoo.gr>
2023-03-16 21:09:08 +0200

    Added New Car

---
+++ ConsoleApplication2/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+	public class Car
+	{
+		string color = "red";
+
+		public Car()
+		{
+
+		}
+		static void Test(string[] args)
+		{
+			Car myObj = new Car();
+			Console.WriteLine(myObj.color);
+		}
+	}
+}
